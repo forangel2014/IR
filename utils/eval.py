42dbf6dfee707805a9868b744d96c33a1114d752
forangel2014 <swt17@mails.tsinghua.edu.cn>
2021-12-28 03:06:27 +0000

    modify

--- utils/eval.py
+++ utils/eval.py
@@ -1,7 +1,10 @@
 import os
 
-def eval(test_dir='./test_results/BM25/', eval_dir='./eval_results/BM25/'):
-    for filename in os.listdir(test_dir):
-        os.system('./trec_eval-9.0.7/trec_eval -m ndcg_cut ./data/2020qrels-pass.txt ' +
-                    test_dir + filename + ' > ' + eval_dir + filename)
-    print('evaluation completed.')
+def eval(test_dir='./test_results/', eval_dir='./eval_results/', sys_names=['Bert', 'DPR', 'BM25']):
+    for sys_name in sys_names:
+        test_sys_dir = test_dir + sys_name + '/'
+        eval_sys_dir = eval_dir + sys_name + '/'
+        for filename in os.listdir(test_sys_dir):
+            os.system('./trec_eval-9.0.7/trec_eval -m ndcg_cut ./data/2020qrels-pass.txt ' +
+                        test_sys_dir + filename + ' > ' + eval_sys_dir + filename)
+        print('evaluation completed.')
